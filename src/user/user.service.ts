2542924ef9ace90ed3193bf34ad274e654191933
Pedro <phfs28@gmail.com>
2022-02-01 20:17:04 -0300

    Created token and login methods

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import * as bcrypt from 'bcrypt';
 import { Prisma } from '@prisma/client';
@@ -7,7 +7,7 @@ import { Prisma } from '@prisma/client';
 export class UserService {
   constructor(private prisma: PrismaService) {}
 
-  async get(id: number) {
+  async get(id: number, hash = false) {
 
     id = Number(id);
     
@@ -24,7 +24,9 @@ export class UserService {
       }
     });
 
-    delete user.password;
+    if (!hash) {
+      delete user.password;
+    }
 
     if (!user) {
       throw new NotFoundException('User not found');
@@ -186,4 +188,19 @@ export class UserService {
     
     return this.get(id);
   }
+
+  async checkPassword(id: number, password: string) {
+
+    const user = await this.get(id, true);
+
+    const checked = await bcrypt.compare(password, user.password);
+
+    if (!checked) {
+      throw new UnauthorizedException('Email or password is incorrect');
+    }
+
+    return true;
+
+  }
+
 }
