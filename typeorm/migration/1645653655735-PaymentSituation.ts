b3fd897834b61a1c2fce740c09c32f9a28ca3c14
Pedro Henrique <phfs28@gmail.com>
2022-02-24 20:18:49 -0300

    Created payment situations seeder

--- typeorm/migration/1645653655735-PaymentSituation.ts
+++ typeorm/migration/1645653655735-PaymentSituation.ts
@@ -18,6 +18,19 @@ export class PaymentSituation1645653655735 implements MigrationInterface {
                 columnUpdatedAt,
             ],
         }));
+
+        const PAYMENT_SITUATIONS = [
+            'Aguardando Pagamento',
+            'Cancelado',
+            'Pagamento Aprovado',
+            'Pagamento Estornado',
+            'Em mediação',
+            'Enviado',
+        ];
+
+        PAYMENT_SITUATIONS.forEach(async (situation) => {
+            await queryRunner.query(`INSERT INTO payment_situations(name) VALUES ('${situation}')`);
+        })
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
